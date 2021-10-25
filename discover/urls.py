03c8209868433552f33c09f945c99222c76f334e
laila-ba <lailabahmani100@gmail.com>
2021-10-25 21:58:49 +0000

    added film urls and views for templates + btn styling

--- discover/urls.py
+++ discover/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_films, name='films'),
-    path('', views.all_shorts, name='shorts'),
+    path('films/', views.all_films, name='films'),
+    path('shorts/', views.all_shorts, name='shorts'),
+    path('luca/', views.film_templates, name='luca'),
+    path('bao/', views.short_templates, name='bao'),
 ]
--- discover/views.py
+++ discover/views.py
@@ -4,10 +4,14 @@ from django.shortcuts import render
 
 def all_films(request):
     """ A view that renders the films contents page """
-
     return render(request, 'discover/films.html')
+
+def film_templates(request):
+        return render(request, 'discover/luca.html')
 
 def all_shorts(request):
     """ A view that renders the shorts contents page """
-
     return render(request, 'discover/shorts.html')
+
+def short_templates(request):
+        return render(request, 'discover/bao.html')
